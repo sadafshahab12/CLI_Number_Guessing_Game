1eb5b9c23c2a58128be4cca338e5079e591703c6
binteshahab12 <159866807+binteshahab12@users.noreply.github.com>
2024-03-26 15:15:13 +0500

    push numbre guessing game command

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Guess the number among 1 to 10:",
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Hurrah! You Guessed the right number");
+}
+else {
+    console.log("Guess Wrong, Game End!");
+}
